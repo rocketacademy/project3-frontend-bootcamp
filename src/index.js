ac37dde0446db70f29ebf0b80ed7fd8660b6dc97
adamvader <adamonglt@hotmail.com>
2022-10-13 21:31:43 +0800

    casing for reservation.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "reservations",
+        path: "Reservations",
         element: <Reservations />,
       },
     ],
