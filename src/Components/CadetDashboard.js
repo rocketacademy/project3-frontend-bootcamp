2c9ac6320b0e0a0d89a60049a38b9fc3077fb0b4
Jermaine-ye <jer.jmye@gmail.com>
2022-11-21 21:43:11 +0800

    Cadet Dashboard

---
+++ src/Components/CadetDashboard.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { useState } from 'react';
+import {
+  AppShell,
+  Burger,
+  Header,
+  MediaQuery,
+  Navbar,
+  Text,
+  useMantineTheme,
+} from '@mantine/core';
+import Gitbook from './Gitbook';
+import Schedule from './Schedule';
+import LandingPage from './LandingPage';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+
+const CadetDashboard = () => {
+  const [opened, setOpened] = useState(false);
+  // const theme = useMantineTheme();
+
+  return (
+    <Router>
+      <AppShell
+        // navbarOffsetBreakpoint controls when navbar should no longer be offset with padding-left
+        navbarOffsetBreakpoint="sm"
+        // fixed prop on AppShell will be automatically added to Header and Navbar
+        fixed
+        navbar={
+          <Navbar
+            padding="md"
+            // Breakpoint at which navbar will be hidden if hidden prop is true
+            hiddenBreakpoint="sm"
+            // Hides navbar when viewport size is less than value specified in hiddenBreakpoint
+            hidden={!opened}
+            // when viewport size is less than theme.breakpoints.sm navbar width is 100%
+            // viewport size > theme.breakpoints.sm – width is 300px
+            // viewport size > theme.breakpoints.lg – width is 400px
+            width={{ sm: 400, lg: 300 }}
+          >
+            <Navbar.Section>
+              <Text>hello this is title</Text>
+            </Navbar.Section>
+            <Navbar.Section grow mt="lg">
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <Text component={Link} variant="link" to="/">
+                  Home Page
+                </Text>
+
+                <Text component={Link} variant="link" to="/gitbook">
+                  Gitbook
+                </Text>
+                <Text component={Link} variant="link" to="/schedule">
+                  Schedule
+                </Text>
+              </div>
+            </Navbar.Section>
+            <Navbar.Section>
+              <Text>Footer</Text>
+              {/* Can include the logout button here */}
+            </Navbar.Section>
+          </Navbar>
+        }
+        header={
+          <Header height={70} padding="md">
+            {/* Handle other responsive styles with MediaQuery component or createStyles function */}
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size="sm"
+                  // color={theme.colors.gray[6]}
+                  mr="xl"
+                />
+              </MediaQuery>
+
+              <Text>Application header</Text>
+            </div>
+          </Header>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/gitbook" element={<Gitbook />} />
+          <Route path="/schedule" element={<Schedule />} />
+          {/* <Route path="/calendarPage" element={<CalendarExample />} />
+            <Route path="/timeInputPage" element={<TimeInputExample />} />
+            <Route path="/notificationPage" element={<NotificationExample />} /> */}
+        </Routes>
+      </AppShell>
+    </Router>
+  );
+};
+
+export default CadetDashboard;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+// import { Auth0Provider } from '@auth0/auth0-react';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  //   <Auth0Provider
+  //   domain={process.env.REACT_APP_DOMAIN}
+  //   clientId={process.env.REACT_APP_CLIENT_ID}
+  //   redirectUri={process.env.REACT_APP_REDIRECT}
+  //   audience={process.env.REACT_APP_AUDIENCE}
+  //   scope={process.env.REACT_APP_SCOPE}
+  // >
+
+  <App />
+
+  // </Auth0Provider>
+);
