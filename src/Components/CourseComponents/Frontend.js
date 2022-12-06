79ea6a419e17b722e60a7265bed7b8cafa0c0331
adamvader <adamonglt@hotmail.com>
2022-12-06 23:36:16 +0800

    fixed button color

--- src/Components/CourseComponents/Frontend.js
+++ src/Components/CourseComponents/Frontend.js
@@ -1,39 +1,40 @@
-import { useState, useEffect } from 'react';
-import { Modal, Button, Group } from '@mantine/core';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { Modal, Button, Group } from "@mantine/core";
+import axios from "axios";
 
-import { BACKEND_URL } from '../../constants.js';
-import DisplayMarkdown from '../DisplayMarkdown';
+import { BACKEND_URL } from "../../constants.js";
+import DisplayMarkdown from "../DisplayMarkdown";
 // import api from '../../api/materials';
-import Forum from '../Forum';
+import Forum from "../Forum";
 
-import { IconAlertCircle } from '@tabler/icons';
-import StarMapButtonDown from '../StarMapButtonDown';
-import StarMapButtonUP from '../StarMapButtonUP';
+import { IconAlertCircle } from "@tabler/icons";
+import StarMapButtonDown from "../StarMapButtonDown";
+import StarMapButtonUP from "../StarMapButtonUP";
 
 function ModalDemo() {
   const [opened, setOpened] = useState(false);
 
-  const [done, setDone] = useState('');
-  const [btn1, setBtn1] = useState('');
+  const [done, setDone] = useState("");
+  const [btn1, setBtn1] = useState("");
   const [btn1color, setBtn1Color] = useState([]);
-  const [btn2, setBtn2] = useState('');
+  const [btn2, setBtn2] = useState("");
   const [btn2color, setBtn2Color] = useState([]);
-  const [btn3, setBtn3] = useState('');
-  const [btn4, setBtn4] = useState('');
-  const [btn5, setBtn5] = useState('');
-  const [btn6, setBtn6] = useState('');
-  const [btn7, setBtn7] = useState('');
-  const [btn8, setBtn8] = useState('');
-  const [btn9, setBtn9] = useState('');
-  const [btn10, setBtn10] = useState('');
-  const [btn11, setBtn11] = useState('');
-  const [btn12, setBtn12] = useState('');
+  const [btn3, setBtn3] = useState("");
+  const [btn4, setBtn4] = useState("");
+  const [btn5, setBtn5] = useState("");
+  const [btn6, setBtn6] = useState("");
+  const [btn7, setBtn7] = useState("");
+  const [btn8, setBtn8] = useState("");
+  const [btn9, setBtn9] = useState("");
+  const [btn10, setBtn10] = useState("");
+  const [btn11, setBtn11] = useState("");
+  const [btn12, setBtn12] = useState("");
   const [sectionId, setSectionId] = useState(5);
   const [urlNum, setUrlNum] = useState(1);
-  const [btnNum, setBtnNum] = useState('');
+  const [btnNum, setBtnNum] = useState("");
   const [cadetId, setCadetId] = useState(1);
   // const [url, setUrl] = useState('');
+  const [completedChaps, setCompletedChaps] = useState({});
 
   useEffect(() => {
     const fetchChapters = async () => {
@@ -43,7 +44,7 @@ function ModalDemo() {
         );
 
         // const response = await api.get('/chapters');
-        console.log('chapters', response.data);
+        console.log("chapters", response.data);
 
         setBtn1(response.data[0]);
         setBtn2(response.data[1]);
@@ -62,10 +63,14 @@ function ModalDemo() {
         const response2 = await axios.get(
           `${BACKEND_URL}/cadetChapters/progress-status?cadetId=${cadetId}`
         );
-        console.log('res2', response2.data);
-        setBtn1Color(response2.data[0].completed);
-        setBtn2Color(response2.data[1].completed);
-        console.log('btn1 color', response2.data[0].completed);
+        console.log("res2", response2.data);
+        let chapsCompleted = {};
+
+        for (let i = 0; i < response2.data.length; i++) {
+          chapsCompleted[response2.data[i].chapterId] = true;
+        }
+
+        setCompletedChaps(chapsCompleted);
       } catch (err) {
         console.log(err.response.data);
       }
@@ -111,88 +116,88 @@ function ModalDemo() {
         <div className="Chapter-1-btn">
           <StarMapButtonUP
             setOpened={setOpened}
-            color={btn1color ? 'blue' : 'gray'}
-            btnName="HTML lala"
+            color={completedChaps[btn1.id] ? "blue" : "gray"}
+            btnName={btn1.chapterIndex}
             topic={btn1.name}
           />
         </div>
         <div className="Chapter-2-btn">
           <StarMapButtonDown
             setOpened={setOpened}
-            color={btn2color ? 'blue' : 'gray'}
-            btnName={btn2.id}
+            color={completedChaps[btn2.id] ? "blue" : "gray"}
+            btnName={btn2.chapterIndex}
             topic={btn2.name}
           />
         </div>
         <div className="Chapter-3-btn">
           <StarMapButtonUP
             setOpened={setOpened}
-            color={btn3 ? 'blue' : 'gray'}
-            btnName={btn3.id}
+            color={completedChaps[btn3.id] ? "blue" : "gray"}
+            btnName={btn3.chapterIndex}
             topic={btn3.name}
           />
         </div>
         <div className="Chapter-4-btn">
           <StarMapButtonDown
             setOpened={setOpened}
-            color={btn4.completed ? 'blue' : 'gray'}
-            btnName={btn4.id}
+            color={completedChaps[btn4.id] ? "blue" : "gray"}
+            btnName={btn4.chapterIndex}
             topic={btn4.name}
           />
         </div>
         <div className="Chapter-5-btn">
           <StarMapButtonDown
             setOpened={setOpened}
-            color={btn5.completed ? 'blue' : 'grey'}
-            btnName={btn5.point}
-            topic={btn5.topic}
+            color={completedChaps[btn5.id] ? "blue" : "grey"}
+            btnName={btn5.chapterIndex}
+            topic={btn5.name}
           />
         </div>
         <div className="Chapter-6-btn">
           <StarMapButtonUP
             setOpened={setOpened}
-            color={btn6.completed ? 'blue' : 'gray'}
-            btnName={btn6.id}
+            color={completedChaps[btn6.id] ? "blue" : "gray"}
+            btnName={btn6.chapterIndex}
             topic={btn6.name}
           />
         </div>
         <div className="Chapter-7-btn">
           <StarMapButtonDown
             setOpened={setOpened}
-            color={btn7.completed ? 'blue' : 'gray'}
-            btnName={btn7.id}
+            color={completedChaps[btn7.id] ? "blue" : "gray"}
+            btnName={btn7.chapterIndex}
             topic={btn7.name}
           />
         </div>
         <div className="Chapter-8-btn">
           <StarMapButtonUP
             setOpened={setOpened}
-            color={btn8.completed ? 'blue' : 'gray'}
-            btnName={btn8.id}
+            color={completedChaps[btn8.id] ? "blue" : "gray"}
+            btnName={btn8.chapterIndex}
             topic={btn8.name}
           />
         </div>
         <div className="Chapter-9-btn">
           <StarMapButtonUP
             setOpened={setOpened}
-            color={btn9.completed ? 'blue' : 'gray'}
-            btnName={btn9.id}
+            color={completedChaps[btn9.id] ? "blue" : "gray"}
+            btnName={btn9.chapterIndex}
             topic={btn9.name}
           />
         </div>
         <div className="Chapter-10-btn">
           <StarMapButtonDown
             setOpened={setOpened}
-            color={btn10.completed ? 'blue' : 'gray'}
-            btnName={btn10.id}
+            color={completedChaps[btn10.id] ? "blue" : "gray"}
+            btnName={btn10.chapterIndex}
             topic={btn10.name}
           />
         </div>
         <div className="Chapter-11-btn">
           <StarMapButtonDown
             setOpened={setOpened}
-            color={btn11.completed ? 'blue' : 'gray'}
-            btnName={btn11.id}
+            color={completedChaps[btn11.id] ? "blue" : "gray"}
+            btnName={btn11.chapterIndex}
             topic={btn11.name}
           />
         </div>
