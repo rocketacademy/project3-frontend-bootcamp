2fc79764f8a7c8cb06a865235fce7607811df0d6
weiyu95 <weiyu95@gmail.com>
2022-12-07 14:52:27 +0800

    able to query backend for single chapter entry and display markdown file.

--- src/Components/DisplayMarkdown.js
+++ src/Components/DisplayMarkdown.js
@@ -13,34 +13,24 @@ import { Modal } from "@mantine/core";
 
 const MARKDOWN_FOLDER_NAME = "markdown";
 
-function DisplayMarkdown() {
+function DisplayMarkdown(markdown) {
   const [post, setPost] = useState("");
   const [isDark, setIsDark] = useState(true);
-  const url =
-    "https://raw.githubusercontent.com/weiyu95/bootcamp3.0-docs/main/1-frontend/1.2-css/1.2.1-layout.md";
-  const test =
-    "https://firebasestorage.googleapis.com/v0/b/launchpad-app-development.appspot.com/o/markdown%2F1.1_%20HTML%20-%20text.txt?alt=media&token=f39511a9-b5b1-4975-be66-36a58e4fa614";
-  const [firebaseUrl, setFirebaseUrl] = useState("");
 
   useEffect(() => {
-    // import(
-    //   "https://firebasestorage.googleapis.com/v0/b/launchpad-app-development.appspot.com/o/markdown%2F1.1_%20HTML%20-%20text.txt?alt=media&token=f39511a9-b5b1-4975-be66-36a58e4fa614"
-    // ).then((res) => {
-    //   fetch(res.default)
-    //     .then((response) => response.text())
-    //     .then((response) => setPost(response))
-    //     .catch((err) => console.log(err));
-    // });
-    // const fileRef = storageRef(storage, `${MARKDOWN_FOLDER_NAME}/1.1_HTML.md`);
-    // getDownloadURL(fileRef).then((downloadUrl) => {
-    //   setFirebaseUrl(downloadUrl);
-    // });
-    // console.log(firebaseUrl);
-    axios.get(url).then((res) => {
-      setPost(res.data);
-      console.log(res.data);
-    });
-  }, []);
+    if (markdown) {
+      const getProps = async () => {
+        axios.get(markdown.markdown).then((res) => setPost(res.data));
+      };
+      getProps();
+    }
+  }, [markdown]);
+
+  // useEffect(() => {
+  //   axios.get(markdown.markdown).then((res) => {
+  //     setPost(res.data);
+  //   });
+  // }, [markdown]);
 
   console.log(post);
 
--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,9 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Modal, Button, Group } from "@mantine/core";
 import DisplayMarkdown from "./DisplayMarkdown";
+import axios from "axios";
 
 function ModalDemo() {
   const [opened, setOpened] = useState(false);
+  const [frontend, setFrontend] = useState({});
+  // useEffect(() => {
+  //   if (user) {
+  //     const getFrontend = async () => {
+  //       // const accessToken = await getAccessTokenSilently({
+  //       //   audience: "https://stayhere/api",
+  //       //   scope: "read:current_user",
+  //       // });
+  //       axios
+  //         .get(`${BACKEND_URL}/chapters/${chapter_name}`, {
+  //           params: {
+  //             ownerEmail: user.email,
+  //           },
+  //           headers: {
+  //             Authorization: `Bearer ${accessToken}`,
+  //           },
+  //         })
+  //         .then((response) => {
+  //           setFrontend(response.data);
+  //         });
+  //     };
+
+  //     getProperties();
+  //   }
+  // }, [user]);
+
+  useEffect(() => {
+    const getFrontend = async () => {
+      axios.get("http://localhost:8080/chapters/15").then((response) => {
+        setFrontend(response.data);
+      });
+    };
+    getFrontend();
+  }, []);
+
+  console.log(frontend);
 
   return (
     <>
@@ -14,11 +51,11 @@ function ModalDemo() {
         centered
         size="xl"
       >
-        <DisplayMarkdown />
+        <DisplayMarkdown markdown={frontend.markdownUrl} />
       </Modal>
 
       <Group position="center">
-        <Button onClick={() => setOpened(true)}>Frontend</Button>
+        <Button onClick={() => setOpened(true)}>Layout</Button>
       </Group>
     </>
   );
