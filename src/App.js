620360d2a9a18f062e16a88257fb80151fbb9c5f
Cljwen <carmenlaujwn@gmail.com>
2023-02-05 17:19:19 +0800

    created first base base repo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React from "react";
-import logo from "./logo.png";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import logo from "./assets//images/logo.png";
 import "./App.css";
+import "./assets//fonts/font.css";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Hello.</h2>
+            <p>This is the base repo.</p>
+          </header>
+          {/* <Routes>
+
+          // Navbar goes here.
+            <Route element={<Home />}>
+
+            // Homepage is all listings.
+              <Route path="/listings" element={<AllListings />} />
+
+              //Create new listing
+              <Route path="/listings/new" element={<NewListingPage />} />
+
+              //Specific listing page
+              <Route
+                path="/listings/:listingNumber"
+                element={<ListingPage />}
+              />
+              <Route path="*" element={<Navigate to="/listings" />} />
+            </Route>
+            
+            //Intro w/ sign up/ login page
+            <Route path="*" element={<WelcomePage />} />
+          </Routes> */}
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/pages/IntroPage.js
@@ -0,0 +1,1 @@
+// welcome page with short intro, login and sign up buttons
