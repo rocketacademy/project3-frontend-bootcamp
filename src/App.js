35a9b17c5f8e3334d0d539610438c84cf341f5e6
0mgABear <yeoyi94@gmail.com>
2023-02-07 21:02:57 +0800

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { AuthPage } from "../components/AuthPage";
+import { User } from "../components/User";
+import { EditUser } from "../components/EditUser";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/user/:id" element={<User />} />
+        <Route path="/user/edit" elemetn={<EditUser />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
