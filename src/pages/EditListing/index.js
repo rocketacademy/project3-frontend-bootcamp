1962b4acf85d872118133df89ffb4881e729fbda
longpaladin <frostarcher7@gmail.com>
2023-02-22 01:24:28 +0800

    user profile and edit listing done

--- src/pages/EditListing/index.js
+++ src/pages/EditListing/index.js
@@ -1,13 +1,23 @@
 //edit individual listing
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./editlisting.css";
 import { Navbar } from "../../commoncomponents/Navbar/Navbar";
-import { useNavigate } from "react-router-dom";
-
-import { Button, Input, Upload, Radio, ConfigProvider, Layout } from "antd";
+import {
+  Button,
+  Input,
+  Upload,
+  Radio,
+  ConfigProvider,
+  Layout,
+  Form,
+  notification,
+} from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import { useAuth0 } from "@auth0/auth0-react";
-
+import axios from "axios";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import { UploadWidget } from "./UploadWidget";
+import { SmileOutlined, FrownOutlined } from "@ant-design/icons";
 import {
   Footer,
   Sider,
@@ -18,47 +28,81 @@ import {
 
 export function EditListing() {
   const { logout } = useAuth0();
-  const [editing, setEditing] = React.useState(false);
-  const [editingBio, setEditingBio] = React.useState(false);
-  const [listingName, setListingName] = React.useState(
-    "Not Without Us book (Original)"
-  );
-  const [bio, setBio] = React.useState(
-    "book has browning on top part other than that very good condition."
-  );
+  const [formValues, setFormValues] = useState({});
+  const { getAccessTokenSilently, user } = useAuth0();
+  const [accessToken, setAccessToken] = useState(null);
+   const [api, contextHolder] = notification.useNotification();
+  let { user_id, listing_id } = useParams();
   const navigate = useNavigate();
 
-  function handleEdit() {
-    setEditing(true);
-  }
+  const handleInputChange = (e) => {
+    setFormValues({ ...formValues, [e.target.id]: e.target.value });
+    console.log(formValues);
+  };
 
-  function handleEditBio() {
-    setEditingBio(true);
-  }
+  const handleListingTypeChange = (e) => {
+    setFormValues({ ...formValues, listing_type: e.target.value });
+    console.log(formValues);
+  };
 
-  function handleSave() {
-    setEditing(false);
-  }
-
-  function handleBioSave() {
-    setEditingBio(false);
-  }
-
-  function handleChange(e) {
-    setListingName(e.target.value);
-  }
-
-  function handleBioChange(e) {
-    setBio(e.target.value);
-  }
-
-  const onChange = (e) => {
-    console.log(`radio checked:${e.target.value}`);
+  const handleConditionChange = (e) => {
+    setFormValues({ ...formValues, condition: e.target.value });
+    console.log(formValues);
   };
 
   const handleBackButtonClick = () => {
     navigate(-1);
   };
+
+ const openSuccessNotification = (placement) => {
+   api.info({
+     message: `Yippee!`,
+     description: "Edit listing successful!",
+     placement,
+     icon: (
+       <SmileOutlined
+         style={{
+           color: "green",
+         }}
+       />
+     ),
+   });
+ };
+ const openFailureNotification = (placement) => {
+   api.info({
+     message: `Oh no!`,
+     description: "Edit listing unsuccessful!",
+     placement,
+     icon: (
+       <FrownOutlined
+         style={{
+           color: "red",
+         }}
+       />
+     ),
+   });
+ };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .put(
+        `http://localhost:3000/${user_id}/listings/${listing_id}`,
+        formValues,
+        configs
+      )
+      .then(function (response) {
+        console.log(response);
+        openSuccessNotification("top");
+        
+      })
+      .catch(function (error) {
+        console.log(error);
+        openFailureNotification("top");
+        
+      });
+  };
+
   const footerStyle = {
     textAlign: "center",
     color: "#fff",
@@ -72,12 +116,34 @@ export function EditListing() {
     left: 0,
     bottom: 0,
     width: "100%",
-    // position: 'absolute',
     backgroundColor: "#303841",
     position: "fixed",
   };
+
+  useEffect(() => {
+    if (user && !accessToken) {
+      getAccessTokenSilently().then((jwt) => setAccessToken(jwt));
+    }
+  }, [user]);
+
+  const configs = {};
+  if (accessToken) configs.headers = { Authorization: `Bearer ${accessToken}` };
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/${user_id}/listings/${listing_id}`, configs)
+      .then(function (response) {
+        console.log(response.data);
+        setFormValues(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [accessToken]);
+
   return (
     <>
+      {contextHolder}
       <ConfigProvider
         theme={{
           token: {
@@ -96,64 +162,76 @@ export function EditListing() {
                 <div className="userName">
                   <div className="userDescription">
                     <div className="profile">
-                      <div className="photo">
-                        <img src="" alt="book" />
-
-                        <Upload action="/upload.do" listType="picture-card">
-                          <div>
-                            <PlusOutlined />
-                          </div>
-                        </Upload>
-                      </div>
-                      <h1>
-                        {editing ? (
-                          <div>
-                            <Input
-                              value={listingName}
-                              onChange={handleChange}
-                            />
-                            <Button onClick={handleSave}>Save</Button>
-                          </div>
-                        ) : (
-                          <div onClick={handleEdit}>{listingName}</div>
-                        )}
-                      </h1>
+                      <Form.Item label="Current Listing Photo" name="photo_url">
+                        <img alt="" src={formValues.photo_url} width="150" />
+                        <br></br>
+                        <UploadWidget
+                          setFormValues={setFormValues}
+                          formValues={formValues}
+                        />
+                      </Form.Item>
                     </div>
-                    <Radio.Group onChange={onChange} defaultValue="a">
-                      <Radio.Button value="a">To Give</Radio.Button>
-                      <Radio.Button value="b">To Request</Radio.Button>
+
+                    <Form.Item
+                      label="Item name"
+                      name="item_name"
+                      onChange={handleInputChange}
+                    >
+                      <Input placeholder={formValues.item_name} />
+                    </Form.Item>
+
+                    <Form.Item
+                      label="Description"
+                      name="description"
+                      onChange={handleInputChange}
+                    >
+                      <Input placeholder={formValues.description} />
+                    </Form.Item>
+
+                    <Form.Item
+                      label="Category"
+                      name="category"
+                      onChange={handleInputChange}
+                    >
+                      <Input placeholder={formValues.category} />
+                    </Form.Item>
+                    <div className="filterHeader">Listing type:</div>
+                    <Radio.Group
+                      onChange={handleListingTypeChange}
+                      value={formValues.listing_type}
+                    >
+                      <Radio.Button value="Give">To Give</Radio.Button>
+                      <Radio.Button value="Take">To Take</Radio.Button>
                     </Radio.Group>
                     <div className="filterHeader">Condition:</div>
-                    <Radio.Group onChange={onChange} defaultValue="a">
-                      <Radio.Button value="a">Brand new</Radio.Button>
-                      <Radio.Button value="b">Like new</Radio.Button>
-                      <Radio.Button value="c">Well used</Radio.Button>
+                    <Radio.Group
+                      onChange={handleConditionChange}
+                      value={formValues.condition}
+                    >
+                      <Radio.Button value="Brand New">Brand new</Radio.Button>
+                      <Radio.Button value="Like New">Like new</Radio.Button>
+                      <Radio.Button value="Well Used">Well used</Radio.Button>
                     </Radio.Group>
-                    <div className="filterHeader">Bio:</div>
-                    {editingBio ? (
-                      <div>
-                        <Input
-                          value={bio}
-                          onChange={handleBioChange}
-                          style={{
-                            width: 500,
-                            height: 100,
-                            whiteSpace: "normal",
-                          }}
-                        />
-                        <Button onClick={handleBioSave}>Save</Button>
-                      </div>
-                    ) : (
-                      <div onClick={handleEditBio}>{bio}</div>
-                    )}
-                    <div className="filterHeader">Status:</div>
-                    <Radio.Group onChange={onChange} defaultValue="a">
-                      <Radio.Button value="a">Available</Radio.Button>
-                      <Radio.Button value="b">Taken</Radio.Button>
-                    </Radio.Group>
+
+                    {/* <div className="filterHeader">Status:</div>
+                    <Radio.Group
+                      onChange={handleStatusChange}
+                      defaultValue={formValues.status}
+                    >
+                      <Radio.Button value="Available">Available</Radio.Button>
+                      <Radio.Button value="Taken">Taken</Radio.Button>
+                    </Radio.Group> */}
                     <div className="filterHeader"></div>
-                    <Button type="primary">Save</Button>
-                    <Button onClick={handleBackButtonClick}>Cancel</Button>
+                    <Form.Item>
+                      <Button
+                        type="primary"
+                        htmlType="submit"
+                        onClick={handleSubmit}
+                      >
+                        Save Changes
+                      </Button>
+                      <Link to={`/${user_id}/profile`}>Back to Profile</Link>
+                    </Form.Item>
                   </div>
                 </div>
               </div>
