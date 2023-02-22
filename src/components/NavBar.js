11afc5aa1c82a178f6922afe547082c916f44745
ameeralih <ameerali.hassanali@gmail.com>
2023-02-22 23:56:22 +0800

    navbar complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,32 @@ import React from "react";
 import { Avatar } from "@mui/material";
 import { AuthButton } from "./AuthButton";
 import AppLogo from "../components/applogo.png";
-import { AppBar } from "@mui/material";
+import { AppBar, Toolbar } from "@mui/material";
 import "../css/App.css";
 
 export function NavBar({ login }) {
   return (
-    <AppBar>
-      <img className="logo" alt="logo" src={AppLogo}></img>
-      {login ? (
-        <div>
-          <input className="search-bar" placeholder="Search for people" />
-          <Avatar />
-          <AuthButton login={login} />
-        </div>
-      ) : (
-        <>
-          <AuthButton login={login} />
-        </>
-      )}
+    <AppBar sx={{ bgcolor: "common.white" }}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <img className="logo" alt="logo" src={AppLogo}></img>
+        {login ? (
+          <div>
+            <input className="search-bar" placeholder="Search for people" />
+            <Avatar />
+            <AuthButton login={login} />
+          </div>
+        ) : (
+          <>
+            <AuthButton login={login} />
+          </>
+        )}
+      </Toolbar>
     </AppBar>
   );
 }
