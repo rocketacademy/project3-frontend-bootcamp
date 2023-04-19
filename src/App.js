875a6ef7fedc6486b9a9230763df476701cf4fa9
ivan89kiat <ivan89kiat@gmail.com>
2023-04-19 21:05:17 +0800

    authentication

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Error from "./Components/Error";
 import Profile from "./Components/Profile";
-import QuestionList from "./Components/QuestionList"
+import QuestionList from "./Components/QuestionList";
 import Question from "./Components/Question";
 import Chatroom from "./Components/Chatroom";
-import Classes from "./Components/Classes"; 
+import Classes from "./Components/Classes";
 import Subject from "./Components/Subject";
-import ClassesList from "./Components/ClassesList"; 
+import ClassesList from "./Components/ClassesList";
 import SubjectsList from "./Components/SubjectsList";
 
 class App extends React.Component {
@@ -19,16 +19,16 @@ class App extends React.Component {
         <div className="App">
           <header className="App-header">
             <Routes>
-              <Route path="/" element={<Home />}/>
-              <Route path="/profile/:id" element={<Profile />}/>
-              <Route path="/classes" element={<ClassesList />}/>
-              <Route path="/classes/:id" element={<Classes />}/>
-              <Route path="/subjects" element={<SubjectsList />}/>
-              <Route path="/subjects/:id" element={<Subject />}/>
-              <Route path="/questions" element={<QuestionList />}/>
-              <Route path="/question/:id" element={<Question />}/>
-              <Route path="/chatroom/:id" element={<Chatroom />}/>
-              <Route path="/*" element={<Error />}/>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile/:id" element={<Profile />} />
+              <Route path="/classes" element={<ClassesList />} />
+              <Route path="/classes/:id" element={<Classes />} />
+              <Route path="/subjects" element={<SubjectsList />} />
+              <Route path="/subjects/:id" element={<Subject />} />
+              <Route path="/questions" element={<QuestionList />} />
+              <Route path="/question/:id" element={<Question />} />
+              <Route path="/chatroom/:id" element={<Chatroom />} />
+              <Route path="/*" element={<Error />} />
             </Routes>
           </header>
         </div>
