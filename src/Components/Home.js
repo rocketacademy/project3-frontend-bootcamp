2f720149742b5656b704e80cb1109222f0951801
ivan89kiat <ivan89kiat@gmail.com>
2023-04-20 11:43:57 +0800

    create userprofile and update profile

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { BACKEND_URL } from "../constant";
 
 export default function Home() {
   const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
@@ -24,6 +27,7 @@ export default function Home() {
     <div>
       <div>Welcome to MyEduCloud</div>
       <div>{isAuthenticated ? logoutButton : loginButton}</div>
+      <Link to="/profile">My Profile</Link>
     </div>
   );
 }
