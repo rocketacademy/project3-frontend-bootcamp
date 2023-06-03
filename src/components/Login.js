49641ab899be03ed90832b2f109509513b4fbca1
daniellimss <highdawn01@yahoo.com>
2023-06-03 13:57:22 +0800

    basic outline of login form

---
+++ src/components/Login.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export const Login = () => {
+
+	const handleEmail = () => {
+		//email validation
+		//authentication
+		console.log('email')
+	}
+
+	const handlePassword = () => {
+		//password validation
+		//authentication
+		console.log('password')
+	}
+
+	return (
+		<div>
+			<form>
+				<input
+					type="email"
+					placeholder="please enter your email"
+					name="email"
+					value="email"
+					onChange={handleEmail}
+				/>
+				<br />
+				<input
+					type="password"
+					placeholder="please enter your password"
+					name="password"
+					value="password"
+					onChange={handlePassword}
+				/>
+				<br />
+				<button type="submit">Submit</button>
+
+			</form>
+		</div>
+	)
+}
