d81ed7eee6b6bdfbcbf8c67693a5fa600f646af9
Jhonnynd <jhonnynd@gmail.com>
2023-08-08 11:45:18 +0800

    added theme and fixed overflowing

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 );
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,19 +1,34 @@
 import React from "react";
 import Hero from "../images/hero-image.png";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 const Homepage = () => {
   return (
     <>
       <Box
+        className="hero"
         sx={{
+          position: "relative",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          width: "100vw",
+          width: "100%",
+          overflow: "hidden",
         }}
       >
-        <img src={Hero} alt="hero"></img>
+        <img src={Hero} alt="hero" />
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{
+            position: "absolute",
+            left: "10%",
+            bottom: "0%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          SHOP NOW
+        </Button>
       </Box>
     </>
   );
