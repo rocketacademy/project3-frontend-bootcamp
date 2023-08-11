4f0d9367d62023a289d68dbab536b89d0384ec99
kaisheng1989 <kaisheng1989@gmail.com>
2023-08-11 21:19:59 +0800

    Storaging token in local storage on index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react"; // Import useAuth0 hook
 
 import { theme } from "./theme";
+
+const AppWithAuth = () => {
+  const { isLoading, getTokenSilently } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // After authentication, get the token and store it in local storage
+  getTokenSilently().then((token) => {
+    localStorage.setItem("access_token", token);
+  });
+
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,15 +35,11 @@ root.render(
     domain={process.env.REACT_APP_AUTHO_DOMAIN}
     clientId={process.env.REACT_APP_AUTHO_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: "http://localhost:3000/",
+      redirect_uri: process.env.REACT_APP_REDIRECT_URI,
       audience: process.env.REACT_APP_AUDIENCE,
-      scope:process.env.REACT_APP_SCOPE
+      scope: process.env.REACT_APP_SCOPE,
     }}
   >
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <AppWithAuth />
   </Auth0Provider>
 );
