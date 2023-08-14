0e57e84b6a3d8c822b68df2cf9f6e17f7ef5bc9a
Snowlyst <tclam.snow@gmail.com>
2023-08-15 00:31:03 +0800

    fixing some merges n stuff

--- src/App.js
+++ src/App.js
@@ -11,21 +11,40 @@ import Deals from "./pages/Deals";
 import Delivery from "./pages/Delivery";
 import Product from "./pages/Product";
 import FirebaseUpload from "./Components/FirebaseUpload";
+import Payment from "./pages/Payment";
+import Auth0 from "./pages/Auth0"; // Import your Auth0 component
+import { useAuth0 } from "@auth0/auth0-react";
+import Category from "./pages/Category";
+import Chat from "./pages/Chat";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/deals" element={<Deals />} />
-        <Route path="/delivery" element={<Delivery />} />
-        <Route path="/products/:productId" element={<Product />} />
-        <Route path="/firebaseupload" element={<FirebaseUpload />} />
+        {isAuthenticated ? (
+          <>
+            {/* Authenticated routes */}
+            <Route path="/" element={<Homepage />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/categories/:categoryId" element={<Category />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/deals" element={<Deals />} />
+            <Route path="/delivery" element={<Delivery />} />
+            <Route path="/products/:productId" element={<Product />} />
+            <Route path="/firebaseUpload" element={<FirebaseUpload />} />
+            <Route path="/payment" element={<Payment />} />
+          </>
+        ) : (
+          <>
+            {/* Unauthenticated route */}
+            <Route path="/" element={<Auth0 />} />
+            <Route path="/chat" element={<Chat />} />
+          </>
+        )}
       </Routes>
     </div>
   );
