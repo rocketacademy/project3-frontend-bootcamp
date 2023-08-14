2fd7664e050e795eed4b45dec69e4e4315e16d0d
Snowlyst <tclam.snow@gmail.com>
2023-08-15 00:23:44 +0800

    merging paths

--- src/App.js
+++ src/App.js
@@ -10,42 +10,23 @@ import Profile from "./pages/Profile";
 import Deals from "./pages/Deals";
 import Delivery from "./pages/Delivery";
 import Product from "./pages/Product";
-import FirebaseUpload from "./Components/FirebaseUpload";
-import Payment from "./pages/Payment";
-import Auth0 from "./pages/Auth0"; // Import your Auth0 component
-import { useAuth0 } from "@auth0/auth0-react";
-import Category from "./pages/Category";
-import Chat from "./pages/Chat";
+import FirebaseUpload from "./Components/firebaseUpload";
 
 function App() {
-  const { isAuthenticated } = useAuth0();
   return (
     <div>
       <Navbar />
       <Routes>
-        {isAuthenticated ? (
-          <>
-            {/* Authenticated routes */}
-            <Route path="/" element={<Homepage />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/categories/:categoryId" element={<Category />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/deals" element={<Deals />} />
-            <Route path="/delivery" element={<Delivery />} />
-            <Route path="/products/:productId" element={<Product />} />
-            <Route path="/firebaseUpload" element={<FirebaseUpload />} />
-            <Route path="/payment" element={<Payment />} />
-          </>
-        ) : (
-          <>
-            {/* Unauthenticated route */}
-            <Route path="/" element={<Auth0 />} />
-            <Route path="/chat" element={<Chat />} />
-          </>
-        )}
+        <Route path="/" element={<Homepage />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/deals" element={<Deals />} />
+        <Route path="/delivery" element={<Delivery />} />
+        <Route path="/products/:productId" element={<Product />} />
       </Routes>
+      <FirebaseUpload />
     </div>
   );
 }
