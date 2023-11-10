91016828c0804761f93ea2ec92d49d3c286c77d2
gbrllim <gbrllim@gmail.com>
2023-11-11 03:12:03 +0800

    Revert "Added default text color to our white, Added standardised pages, built out remaining pages template, built finish errors page"
    This reverts commit 6823ff3390ab3b2faf3bfbc6b4a409dff3cd5f11.

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,4 @@
 //-----------Libaries-----------//
-import { motion } from "framer-motion";
-import { NavLink } from "react-router-dom";
 
 //-----------Components-----------//
 import Button from "../Components/Details/Button";
@@ -10,28 +8,20 @@ import RotatingWords from "../Components/Home/RotatingWords";
 //-----------Media-----------//
 import logo from "../Images/Logo-GitHired.svg";
 import demo from "../Images/Mock-demo.png";
+import "./HomePage.css";
 
 export default function HomePage() {
   const rotatingText = ["Applications", "Reminders", "Questions", "Notes"];
 
   return (
-    <motion.div
-      className=" flex h-screen flex-row items-center justify-center bg-background"
-      initial={{ opacity: 0.2, scale: 1 }} // Initial state (hidden and scaled down)
-      animate={{ opacity: 1, scale: 1 }} // Final state (visible and at full scale)
-      transition={{
-        duration: 0.8,
-        ease: "easeInOut",
-      }}
-    >
+    <div className=" flex h-screen flex-row items-center justify-center bg-background">
       <main className="flex w-1/2 flex-col items-center justify-center">
         <img src={logo} className="scale-125" alt="GitHired Logo" />
         <RotatingWords words={rotatingText} />
-        <form className="mb-4">
+        <form>
           <InputText placeholder="Enter your email" />
           <Button label="Enter" />
         </form>
-        <NavLink to="/dashboard">Try our App</NavLink>
       </main>
       <article className=" flex h-full w-1/2 items-center justify-center bg-primary">
         <img
@@ -41,6 +31,6 @@ export default function HomePage() {
         />
       </article>
       <footer></footer>
-    </motion.div>
+    </div>
   );
 }
--- src/Providers/RouterProvider.js
+++ src/Providers/RouterProvider.js
@@ -4,12 +4,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 //-----------Pages-----------//
 import HomePage from "../Pages/HomePage";
 import OnboardingPage from "../Pages/OnboardingPage";
-import DashboardPage from "../Pages/DashboardPage";
-import NewApplicationPage from "../Pages/NewApplicationPage";
-import PracticePage from "../Pages/PracticePage";
-import MetricsPage from "../Pages/MetricsPage";
-import SettingsPage from "../Pages/SettingsPage";
-import ContactsPage from "../Pages/ContactsPage";
 import ErrorPage from "../Pages/ErrorPage";
 
 const router = createBrowserRouter([
@@ -21,32 +15,6 @@ const router = createBrowserRouter([
   {
     path: "/onboarding",
     element: <OnboardingPage />,
-  },
-  {
-    path: "/dashboard",
-    element: <DashboardPage />,
-    children: [
-      {
-        path: "add",
-        element: <NewApplicationPage />,
-      },
-    ],
-  },
-  {
-    path: "/practice",
-    element: <PracticePage />,
-  },
-  {
-    path: "/metrics",
-    element: <MetricsPage />,
-  },
-  {
-    path: "/settings",
-    element: <SettingsPage />,
-  },
-  {
-    path: "/contacts",
-    element: <ContactsPage />,
   },
 ]);
 
