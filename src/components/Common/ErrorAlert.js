34cdc43a853560540449a83f430367fea7206e11
ianlauyin <ianlauyin@gmail.com>
2024-03-25 22:25:31 +0800

    Refactor ErrorAlert

--- src/components/Common/ErrorAlert.js
+++ src/components/Common/ErrorAlert.js
@@ -11,15 +11,12 @@ const ErrorAlert = ({ message }) => {
     return () => clearTimeout(timer);
   }, []);
 
-  return isVisible ? (
-    <div className="fixed top-0 right-0 m-4 z-50 transition-all duration-500 ease-in-out transform translate-x-0 opacity-100">
-      <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4">
-        <p className="font-bold">Be Warned</p>
-        <p>{message}</p>
-      </div>
-    </div>
-  ) : (
-    <div className="fixed top-0 right-0 m-4 z-50 transition-all duration-500 ease-in-out transform translate-x-full opacity-0">
+  return (
+    <div
+      className={`fixed top-0 right-0 m-4 z-50 transition-all duration-500 ease-in-out transform ${
+        isVisible ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"
+      } `}
+    >
       <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4">
         <p className="font-bold">Be Warned</p>
         <p>{message}</p>
